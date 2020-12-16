6e8fdd90bc9ad8c44c96980f88108e188581b18d
kanon-tencho <kanon@kanon-k4.com>
2020-12-17 04:57:36 +0900

    docker環境構築

---
+++ docker/mysql/init.d/init.sql
@@ -0,0 +1,20 @@
+USE ges;
+
+ALTER USER 'ginlab'@'%' IDENTIFIED WITH mysql_native_password BY 'ginlab';
+
+CREATE TABLE IF NOT EXISTS `enterlog`
+(
+    id INTEGER AUTO_INCREMENT,
+    date DATETIME NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    studentid VARCHAR(128) NOT NULL,
+    enter INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS `inroom`
+(
+    studentid VARCHAR(128) NOT NULL,
+    date DATETIME NOT NULL,
+    PRIMARY KEY(studentid)
+);
