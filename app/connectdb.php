8f68db88e4b7b3e436576b7d41c43d552fa93c20
kanon-tencho <kanon@kanon-k4.com>
2020-12-17 11:34:57 +0900

    connect db on php

--- app/connectdb.php
+++ app/connectdb.php
@@ -1,26 +1,30 @@
 <?php
 # CONNECT
-try {
 
-    $pdo = new PDO(
-            'mysql:dbname=ges;host=localhost;charset=utf8mb4', 
-            'gesystem', 
-            'gesystem', 
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ]);
+function connectdb(){
+    try {
 
-} catch (PDOException $e) {
+        $pdo = new PDO(
+                'mysql:dbname=mysql;host=mysql;charset=utf8mb4', 
+                'root', 
+                'root', 
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                ]);
 
-    // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
-    // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
-    // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
-    header('Content-Type: text/plain; charset=UTF-8', true, 500);
-    exit($e->getMessage()); 
+    } catch (PDOException $e) {
 
+        // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
+        // - もし手抜きしたくない場合は普通にHTMLの表示を継続する
+        // - ここではエラー内容を表示しているが， 実際の商用環境ではログファイルに記録して， Webブラウザには出さないほうが望ましい
+        header('Content-Type: text/plain; charset=UTF-8', true, 500);
+        exit($e->getMessage()); 
+
+    }
 }
 
+connectdb();
 header('Content-Type: text/html; charset=utf-8');
 
 ?>
